4053b8c21c8d7c05c9949b5db763e922639f024b
jlbrown5-ncat <60116362+jlbrown5-ncat@users.noreply.github.com>
2020-05-18 22:33:14 -0400

    Corrected detector deletion bug

---
+++ panda/src/database.py
@@ -0,0 +1,46 @@
+#Connect to MongDB
+#Instantiate object
+
+from pymongo import MongoClient
+from pprint import pprint
+import numpy as np
+import math
+
+class MongoDBConnect:
+
+
+
+    def __init__(self):
+        # connect to MongoDB
+        client = MongoClient("mongodb://localhost:27017/COMP895-Brown?readPreference=primary&appname=MongoDB%20Compass&ssl=false")
+        self.db = client["blacksite"]
+        self.col = self.db["detectors"]
+
+    def get_all(self):
+        temp = list(self.col.find())
+        detectors = {}
+        for d in temp:
+            temp = {'_id': d['_id'], "VALUE": d['VALUE'], "TYPE": d['TYPE'], 'LIFE': d['LIFE']}
+            detectors[d["_id"]] = temp
+
+        return detectors
+
+    def get_one(self, _id):
+        sample = np.array(list(self.col.find({"_id" : _id})))
+        return sample
+
+    def update_detector(self, detector):
+
+        myquery = {"_id": detector["_id"]}
+        newvalues = {"$set": {"TYPE": detector["TYPE"]}}
+        self.col.update_one(myquery, newvalues)
+
+        print("Database update successful")
+
+    def add_detector(self, detector):
+        d = self.col.insert_one(detector)
+        return d
+
+    def delete_detector(self, detector):
+        d = self.col.delete_one(detector)
+        return d
--- panda/src/main.py
+++ panda/src/main.py
@@ -1,158 +1,109 @@
-from fedatabase import MongoDBConnect
+from database import MongoDBConnect
+import random
+import time
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.preprocessing import normalize
 
-THRESHOLD = 0.0000000005
-column_number = []
+MAX_DETECTORS = 1000
+DETECTOR_LENGTH = 32
+INITIAL_DETECTOR_LIFESPAN = 3600
+IMMATURE_DETECTOR_LIFESPAN = 604800
+MATURE_DETECTOR_LIFESPAN = 2592000
+MEMORY_DETECTOR_LIFESPAN = 31536000
+CURRENT_DETECTORS = ""
 
-def filterColumns(column_names, selected_columns):
-    i = 0
+DB = MongoDBConnect()
 
-    temp = []
-    for i in range(len(column_names)):
-        if selected_columns[i] == True:
-            temp.append(column_names[i])
-            column_number.append(i)
-        i += 1
+def generate_detector():
+    # Variable to store the
+    # string
+    value = ""
 
-    return temp
+    # Loop to find the string
+    # of desired length
+    for i in range(DETECTOR_LENGTH):
+        # randint function to generate
+        # 0, 1 randomly and converting
+        # the result into str
+        temp = str(random.randint(0, 1))
 
-def calculate_scores(all_output, all_institutions):
-    scores = []
+        # Concatenatin the random 0, 1
+        # to the final result
+        value += temp
 
-    for i in range(len(all_output)):
-        inst = {}
-        inst['_id'] = all_institutions[i]['_id']
-        x = all_output[i]
-        sum = 0.0
-        for value in x:
-            sum += value
+    detector = {"VALUE": value, "TYPE": "INITIAL", 'LIFE': time.time()}
 
-        inst['SCORE'] = sum
-        scores.append(inst)
-
-    return scores
-
-def convert_to_feature_vector(samples):
-    temp = []
-
-    for x in samples:
-        current_sample = []
-        for k, v in x.items():
-            if k != '_id' and k != 'SCORE':
-                # Check if the value is a number, if not, set it to None (null)
-
-                if type(v) == type(""):
-                    try:
-                        value = float(v)
-                    except ValueError:
-                        value = 0
-                else:
-                    value = 0
-
-                current_sample.append(value)
-
-        temp.append(current_sample[23:])
-
-    return normalize(temp, norm="max")
+    return detector
 
 
-def get_hbcus(all_institutions):
+def remove_detector(detector):
+    # print(detector['TYPE'] + ' detector removed')
+    DB.delete_detector(detector)
+    del CURRENT_DETECTORS[detector['_id']]
+    add_detector()
 
-    temp = []
 
-    for i in range(len(all_institutions)):
-        x = all_institutions[i]
-        if x["HBCU"] == "1":
-            x["index"] = i
-            temp.append(x)
+def add_detector():
+    temp_detector = generate_detector()
+    d = DB.add_detector(temp_detector)
+    temp_detector['_id'] = d.inserted_id
+    CURRENT_DETECTORS[d.inserted_id] = temp_detector
+    # print('Detector added')
 
-    return temp
 
-def get_hbcu_samples(hbcus, all_samples):
-    temp = []
+def generate_initial_detectors():
+    num_detectors = len(CURRENT_DETECTORS)
+    while (num_detectors < MAX_DETECTORS):
+        add_detector()
+        num_detectors = len(CURRENT_DETECTORS)
+        print('{:.2f}%'.format(float(num_detectors)/MAX_DETECTORS*100))
 
-    for x in hbcus:
-        temp.append(all_samples[x["index"]])
 
-    return temp
+def evaluate_lifespans():
+    num_detectors = len(CURRENT_DETECTORS)
+    while (num_detectors < MAX_DETECTORS):
+        add_detector()
+        num_detectors = len(CURRENT_DETECTORS)
+
+    keys = []
+
+    for k in CURRENT_DETECTORS.keys():
+        keys.append(k)
+
+    sum = 0
+    for key in keys:
+        value = CURRENT_DETECTORS[key]
+        lifetime = time.time() - value['LIFE']
+        if value['TYPE'] == 'INITIAL':
+            if lifetime > INITIAL_DETECTOR_LIFESPAN:
+                remove_detector(value)
+                sum += 1
+        elif value['TYPE'] == 'IMMATURE':
+            if lifetime > IMMATURE_DETECTOR_LIFESPAN:
+                remove_detector(value)
+                sum += 1
+        elif value['TYPE'] == 'MATURE':
+            if lifetime > MATURE_DETECTOR_LIFESPAN:
+                remove_detector(value)
+                sum += 1
+        elif value['TYPE'] == 'MEMORY':
+            if lifetime > MEMORY_DETECTOR_LIFESPAN:
+                remove_detector(value)
+                sum += 1
+
+    if (sum > 0):
+        print('{:d} detectors were replaced'.format(sum))
 
 
 if __name__ == "__main__":
+    CURRENT_DETECTORS = DB.get_all()
 
-    db = MongoDBConnect()
+    print('Generating initial detectors')
+    generate_initial_detectors()
+    print('Finished generating initial detectors\n')
 
-    # Get the all the institutions from the database
-    all_institutions = db.get_all()
-    all_samples = convert_to_feature_vector(all_institutions)
-    # print("Institution Feature Vector Creation Successful")
+    print('Detector lifespan evaluation started')
+    while True:
+        evaluate_lifespans()
 
-    hbcus = get_hbcus(all_institutions)
-    # print("Successfully gathered HBCUS")
 
-    # Normalize values
-    hbcu_samples = get_hbcu_samples(hbcus, all_samples)
-    # print("Successfully gathered HBCU Feature Vectors")
-
-    # Reduce the features
-    selector = VarianceThreshold(THRESHOLD)
-    selector.fit(hbcu_samples)
-    # print("Successfully finished Variance Threshold")
-
-    # Insert reduced feature vectors into output variable
-    all_output = selector.transform(all_samples)
-    # print("Successfully finished Transforming all institutions")
-
-    # #Get column names and selected columns
-    # column_names = db.get_variable_code()
-    # selected_columns = list(selector.get_support())
-    #
-    # #Filter out unused columns
-    # column_names = filterColumns(column_names, selected_columns)
-
-    # print("Number of Institutions: " + str(len(all_samples)) + "\n")
-    # print("Number of HBCUs: " + str(len(hbcu_samples)) + "\n")
-    # print("Threshold: " + str(THRESHOLD) + "\n")
-    # print("Original Number of Variables: " + str(len(hbcu_samples[0])) + "\n")
-    # print("Selected Number of Variables: " + str(len(all_output[0])) + "\n")
-
-    # scores = calculate_scores(all_output, all_institutions)
-    # print("Scores Successfully Calculated for academicyear20182019")
-    #
-    # db.update_scores(scores)
-
-    collection_name = "academicyear20172018"
-    all_institutions = db.get_all_from_collection(collection_name)
-    all_samples = convert_to_feature_vector(all_institutions)
-
-    all_output = selector.transform(all_samples)
-    #print("Successfully finished Transforming all institutions for " + collection_name)
-
-    scores = calculate_scores(all_output, all_institutions)
-    print("Scores Successfully Calculated for " + collection_name)
-
-    db.update_scores_for_collection(collection_name, scores)
-
-    collection_name = "academicyear20162017"
-    all_institutions = db.get_all_from_collection(collection_name)
-    all_samples = convert_to_feature_vector(all_institutions)
-
-    all_output = selector.transform(all_samples)
-    #print("Successfully finished Transforming all institutions for " + collection_name)
-
-    scores = calculate_scores(all_output, all_institutions)
-    print("Scores Successfully Calculated for " + collection_name)
-
-    db.update_scores_for_collection(collection_name, scores)
-
-    collection_name = "academicyear20152016"
-    all_institutions = db.get_all_from_collection(collection_name)
-    all_samples = convert_to_feature_vector(all_institutions)
-
-    all_output = selector.transform(all_samples)
-    #print("Successfully finished Transforming all institutions for " + collection_name)
-
-    scores = calculate_scores(all_output, all_institutions)
-    print("Scores Successfully Calculated for " + collection_name)
-
-    db.update_scores_for_collection(collection_name, scores)
