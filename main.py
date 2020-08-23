a3e8ed29eeeb7fe767cc9cd6f2e22861e38a438e
jlbrown5-ncat <60116362+jlbrown5-ncat@users.noreply.github.com>
2020-08-23 18:26:51 -0400

    Modified blacksite package structure

--- main.py
+++ main.py
@@ -1,13 +1,56 @@
-import deepneuralnetwork as dnn
 import threading
 from concurrent.futures import ThreadPoolExecutor, wait
+from common.database import MongoDBConnect
+import grizzly.grizzly
+import panda.panda
+import polar.polar
+import csv
 import sys
 
 EXECUTOR = ThreadPoolExecutor(10)
 FUTURES = []
 LOCK = threading.Lock()
+DB = MongoDBConnect()
 
 if __name__ == "__main__":
+    option = input('Read from csv file: y/n\n')
+
+    while True:
+        if option == 'y':
+            filename = input('\nFilename to read from\n')
+            with open(filename, 'r') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    FUTURES.append(EXECUTOR.submit(polar.add_new_instance, row))
+            break
+        elif option == 'n':
+            # begin CICFlowmeter-V4.0
+            print('\nStarting CICFlowmeter-V4.0')
+            break
+        else:
+            print('Invalid input')
+            option = input('Read from csv file: y/n\n')
+
+    panda.CURRENT_DETECTORS = DB.get_all_detectors()
+
+    if len(panda.CURRENT_DETECTORS) < panda.MAX_DETECTORS:
+        panda.generate_initial_detectors()
+        print('Generating initial detectors')
+
+    FUTURES.append(EXECUTOR.submit(panda.evaluate_detector_lifespans))
+    print('Evaluating detector lifespans')
+
+    FUTURES.append(EXECUTOR.submit(panda.update_persistent_detectors))
+    print('Responding to detector updates')
+
+    FUTURES.append(EXECUTOR.submit(panda.regenerate_detector))
+    print('Regenerating deleted detectors')
+
+    # FUTURES.append(EXECUTOR.submit(panda.classify_initial_new_instances))
+    print('Classifying currently active new instances')
+
+    #  FUTURES.append(EXECUTOR.submit(panda.classify_new_instances))
+    print('Classifying new instances')
 
     option = input(
         '0: Load Deep Neural Network\n'
@@ -24,10 +67,10 @@ if __name__ == "__main__":
             while True:
                 if file_prompt == 'y':
                     filename = input('\nEnter the csv filename\n')
-                    dnn.DNN = dnn.train_dnn(filename)
+                    grizzly.train_dnn(filename)
                     break
                 elif file_prompt == 'n':
-                    dnn.DNN = dnn.train_dnn()
+                    grizzly.train_dnn()
                     break
                 else:
                     print('\nInvalid input')
@@ -58,7 +101,7 @@ if __name__ == "__main__":
             while True:
                 if save_prompt == 'y':
                     filename = input('\nEnter the Deep Neural Network filename to save to\n')
-                    dnn.save_dnn(filename)
+                    grizzly.save_dnn(filename)
                     sys.exit(0)
                 elif save_prompt == 'n':
                     sys.exit(0)
@@ -72,11 +115,11 @@ if __name__ == "__main__":
                 '1: Exit\n'
             )
 
-    FUTURES.append(EXECUTOR.submit(dnn.retrain_dnn_callback))
-    FUTURES.append(EXECUTOR.submit(dnn.train_initial_detectors))
-    FUTURES.append(EXECUTOR.submit(dnn.retrain_detectors_callback))
-    FUTURES.append(EXECUTOR.submit(dnn.evaluate_initial_suspicious_instances))
-    FUTURES.append(EXECUTOR.submit(dnn.evaluate_suspicious_instances_callback))
+    FUTURES.append(EXECUTOR.submit(grizzly.retrain_dnn_callback))
+    FUTURES.append(EXECUTOR.submit(grizzly.train_initial_detectors))
+    FUTURES.append(EXECUTOR.submit(grizzly.retrain_detectors_callback))
+    FUTURES.append(EXECUTOR.submit(grizzly.evaluate_initial_suspicious_instances))
+    FUTURES.append(EXECUTOR.submit(grizzly.evaluate_suspicious_instances_callback))
 
     wait(FUTURES, return_when='ALL_COMPLETED')
 
@@ -88,7 +131,7 @@ if __name__ == "__main__":
     while True:
         if option == '0':
             filename = input('\nEnter the Deep Neural Network filename to save to\n')
-            dnn.save_dnn(filename)
+            grizzly.save_dnn(filename)
             break
         elif option == 1:
             sys.exit(0)
